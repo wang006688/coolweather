18a6a2c35800506b9a3c89b917b3a4f08cd02d66
wang006688 <wang006688@gmail.com>
2015-09-11 20:23:33 -0700

    First fixed codes

---
+++ app/src/main/java/com/mygame/example/course/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.mygame.example.course.coolweather.model;
+
+/**
+ * Created by Wang on 9/11/2015.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
